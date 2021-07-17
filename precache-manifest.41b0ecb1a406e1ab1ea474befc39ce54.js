2b53f6303323b6704ca2ca198f177e7265c8730e
Agrim77 <sharma.agrim77@gmail.com>
2021-07-17 20:10:35 +0530

    Updates

---
+++ precache-manifest.41b0ecb1a406e1ab1ea474befc39ce54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f66af8abafa3e801630f5600dc8082f",
+    "url": "/DTYE_code_editor_react/index.html"
+  },
+  {
+    "revision": "00fa1cbb1601d1a84fcc",
+    "url": "/DTYE_code_editor_react/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "2a57e82bad60b22af24f",
+    "url": "/DTYE_code_editor_react/static/css/main.2a81af78.chunk.css"
+  },
+  {
+    "revision": "00fa1cbb1601d1a84fcc",
+    "url": "/DTYE_code_editor_react/static/js/2.1c4cfd3b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DTYE_code_editor_react/static/js/2.1c4cfd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a57e82bad60b22af24f",
+    "url": "/DTYE_code_editor_react/static/js/main.e307e0e7.chunk.js"
+  },
+  {
+    "revision": "b11a6354664594b19983",
+    "url": "/DTYE_code_editor_react/static/js/runtime-main.b58c6ac2.js"
+  }
+]);
